d8509e18264104f338368bb862fb8f1ea5da83e5
Sean Monacillo <71977122+Sean032100@users.noreply.github.com>
2020-11-21 09:37:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
-
-echo ' <script src="/public/js/jquery.min.js"></script> -
-    <script src="/public/js/popper.min.js"></script> -
-    <script src="/public/js/bootstrap.min.js"></script> -
-    <script src="/public/js/jquery.easing.min.js"></script> -
-    <script src="/public/js/swiper.min.js"></script> -
-    <script src="/public/js/jquery.magnific-popup.js"></script> - 
-    <script src="/public/js/morphext.min.js"></script> -
-    <script src="/public/js/isotope.pkgd.min.js"></script> -
-    <script src="/public/js/validator.min.js"></script> -
-    <script src="/public/js/scripts.js"></script> -
-    <script src="/public/js/mdb.min.js"></script> -
-    <script src="/public/js/all.js"></script> ';
-
-include_once("mainclient/mainclient.html");
-
-   
+include_once("mainclient/mainclient.html"); 
 ?>
+<script type="text/javascript" src="/public/js/jquery.min.js"></script>
+<script type="text/javascript" src="/public/js/popper.min.js"></script>
+<script type="text/javascript" src="/public/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/public/js/jquery.easing.min.js"></script>
+<script type="text/javascript" src="/public/js/swiper.min.js"></script>
+<script type="text/javascript" src="/public/js/jquery.magnific-popup.js"></script>
+<script type="text/javascript" src="/public/js/morphext.min.js"></script>
+<script type="text/javascript" src="/public/js/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="/public/js/validator.min.js"></script>
+<script type="text/javascript" src="/public/js/scripts.js"></script>
+<script type="text/javascript" src="/public/js/mdb.min.js"></script>
+<script type="text/javascript" src="/public/js/all.js"></script>
